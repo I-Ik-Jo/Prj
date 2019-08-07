bb7f44ad09c13a5871d8e70412349a3c63f6de4e
I-Ik-Jo <bu7203@gmail.com>
2019-08-07 14:18:06 +0900

    Spring seconde

---
+++ SpringBasic/src/main/java/com/spring/board/controller/BoardController.java
@@ -0,0 +1,21 @@
+package com.spring.board.controller;
+
+import java.util.logging.Logger;
+
+import org.eclipse.jdt.internal.compiler.batch.Main;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+
+public class BoardController {
+
+   protected static Logger logger = Logger.getLogger(Main.class.getName());
+	  
+   @RequestMapping("/")
+   public String home() {
+	   logger.info("인터셉터 테스트");
+	   return "home";
+   }
+      
+}
